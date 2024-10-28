199c534b1bfc46192451be977652c68ab2a351a9
AlexanderGardiner <misc@alexandergardiner.com>
2024-10-27 18:13:14 -0700

    Inital commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.ShootIfAble;
+import frc.robot.subsystems.SwerveSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+    private SwerveSubsystem swerveSubsystem;
+
+    private CommandJoystick leftJoystick;
+    private CommandJoystick rightJoystick;
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        swerveSubsystem = new SwerveSubsystem();
+        configureBindings();
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return null;
+    }
+
+    public void configureBindings() {
+
+        leftJoystick = new CommandJoystick(0);
+        rightJoystick = new CommandJoystick(1);
+
+        swerveSubsystem.setDefaultCommand(swerveSubsystem.driveCommand(() -> {
+            return -leftJoystick.getY();
+        }, () -> {
+            return -leftJoystick.getX();
+        }, () -> {
+            return -rightJoystick.getX();
+        }));
+    }
+}
